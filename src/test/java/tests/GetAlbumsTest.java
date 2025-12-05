29e6149d6b194eb9a4dc99555545309d06e12a12
Iqra <iqrahaider_khan@yahoo.com>
2025-12-05 15:00:21 -0500

    Fix BaseTest initialization and TestNG setup

--- src/test/java/tests/GetAlbumsTest.java
+++ src/test/java/tests/GetAlbumsTest.java
@@ -1,23 +1,22 @@
 package tests;
 
 import framework.base.BaseTest;
-import io.restassured.RestAssured;
 import io.restassured.response.Response;
-import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
 
 public class GetAlbumsTest extends BaseTest {
 
     @Test
     public void testGetAlbums() {
-        Response response = RestAssured
-                .given()
-                .when()
-                .get("/albums");
 
-        assertEquals(200, response.getStatusCode());
+        Response response = apiClient.get("albums");
+
+        assertEquals(response.statusCode(), 200, "Status code should be 200");
     }
 }
+
 
 
 
