2b04a56fa029295c2ab3be966a53cd573a7828e7
Iqra <iqrahaider_khan@yahoo.com>
2025-12-07 15:27:13 -0500

    Add test for GET /posts?userId={id} filtering posts by user

---
+++ src/test/java/tests/GetPostsByUserIdTest.java
@@ -0,0 +1,28 @@
+package tests;
+
+import framework.base.BaseTest;
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+public class GetPostsByUserIdTest extends BaseTest {
+
+    @Test
+    public void testGetPostsByUserId() {
+        int userId = 1;
+
+        Response response = apiClient.get("posts?userId=" + userId);
+
+        // Validate status code
+        assertEquals(response.statusCode(), 200, "Status code should be 200");
+
+        // Validate response contains posts
+        assertTrue(response.getBody().asString().contains("\"userId\": 1"),
+                "Response should contain posts for userId = 1");
+    }
+}
+
+
+
