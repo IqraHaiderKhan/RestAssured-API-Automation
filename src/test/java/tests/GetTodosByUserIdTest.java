cccea6375de704cdb7f0555f627bda0f087714ce
Iqra <iqrahaider_khan@yahoo.com>
2025-12-07 15:18:43 -0500

    Add GET /users/{id}/todos API test

---
+++ src/test/java/tests/GetTodosByUserIdTest.java
@@ -0,0 +1,22 @@
+package tests;
+
+import framework.base.BaseTest;
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class GetTodosByUserIdTest extends BaseTest {
+
+    @Test
+    public void testGetTodosByUserId() {
+        int userId = 1;
+
+        Response response = apiClient.get("users/" + userId + "/todos");
+
+        assertEquals(response.statusCode(), 200, "Status code should be 200");
+    }
+}
+
+
+
